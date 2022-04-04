c378b5ba6cd583bec21639e702ad90b2c048b2e7
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2022-04-04 15:15:42 +0200

    Updated module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,10 @@ module no.ntnu.idatg.wargames {
   requires javafx.fxml;
 
   opens no.ntnu.idatg2001.wargames;
+  opens no.ntnu.idatg2001.wargames.model.army;
+  opens no.ntnu.idatg2001.wargames.model.battle;
+  opens no.ntnu.idatg2001.wargames.model.unit;
+  opens no.ntnu.idatg2001.wargames.utility;
   exports no.ntnu.idatg2001.wargames;
   exports no.ntnu.idatg2001.wargames.controller;
   opens no.ntnu.idatg2001.wargames.controller;
