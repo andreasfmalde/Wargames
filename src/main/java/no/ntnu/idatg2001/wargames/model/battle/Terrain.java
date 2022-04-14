c11d6e89da7956843650d41cba900626ee86d5ca
Andreas Follevaag Malde <andrefma@stud.ntnu.no>
2022-04-15 00:29:06 +0200

    Added javadoc to terrain enum

--- src/main/java/no/ntnu/idatg2001/wargames/model/battle/Terrain.java
+++ src/main/java/no/ntnu/idatg2001/wargames/model/battle/Terrain.java
@@ -1,5 +1,12 @@
 package no.ntnu.idatg2001.wargames.model.battle;
 
+/**
+ * Enum to represent the different terrain
+ * types. Battles can take place in
+ * different terrain.
+ * @author Andreas Follevaag Malde
+ * @version 1.0 - SNAPSHOT
+ */
 public enum Terrain {
   HILL,PLAINS,FOREST
 }
